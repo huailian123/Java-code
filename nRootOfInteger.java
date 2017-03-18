bf677a0ac5b663d78df15ebe1732e3c2d7d1d1c7
huailian123 <huailian123@gmail.com>
2017-03-18 10:50:16 -0700

    Update and rename SqrtOfInteger.java to nRootOfInteger.java

--- nRootOfInteger.java
+++ nRootOfInteger.java
@@ -1,3 +1,21 @@
+/* 
+Mar,15,2017
+Output the closest integer(no need for double or float). 
+public static int sqRoot(int x, int n)
+Using binary search i from 1 to x
+Using a helper to calcultate pow(x, n);
+
+Time: o(log(x)))
+Space: o(1)
+*/
+
+/*
+Using search i from 1 to  i ^ n < x
+Time: o(log(x^(1/n))))
+Space: o(1)
+*/
+
+// Tested in IDE
 class Solution {
   
   public static double helper(int x, int n) {
@@ -12,7 +30,7 @@ class Solution {
     }
   }
   
-  public static int sqRoot(int x, int n) {
+  public static int nRoot(int x, int n) {
     return binarySearch(1, x, n);
   }
   
