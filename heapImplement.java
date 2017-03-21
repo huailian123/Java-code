983b67a0c01cd6b77d79f0dfbf8d111859c17744
huailian123 <huailian123@gmail.com>
2017-03-20 23:05:10 -0700

    Update heapImplement.java

--- heapImplement.java
+++ heapImplement.java
@@ -1,26 +1,28 @@
-public class LianPQ <E>｛
-  private E[] array;
-  private size;
-  
-  
-  public int size() {  //date length stored inside
-    return size;
-  }
-  public int isEmpty() {
-    return size == 0;
-  }
-  public LianPQ(int inputSize) {
-    array = new E[inputSize];
-    size = 0;
-  }
-  
-  public LianPQ(E[] inputArray) {
-    array = inputArray;
-    size = inputArray.length;
-    heapify();
-  }
-  
-  public offer(E elem){
+public class LianPQ< E extends Comparable<E>> { // has to be "extends"
+	private Object[] array;  //can't creat E[] array, do not have E constructor
+
+	private int size;
+
+	public int size() { // date length stored inside
+		return size;
+	}
+
+	public boolean isEmpty() {
+		return size == 0;
+	}
+
+	public LianPQ(int inputSize) {
+		array = new Object[inputSize];
+		size = 0;
+	}
+
+	public LianPQ(E[] inputArray) {
+		array = inputArray;
+		size = inputArray.length;
+		heapify();
+	}
+
+	public void offer(E elem){
 //     if (size == array.length) {
 //       resize();
 //     } else {
@@ -30,69 +32,69 @@ public class LianPQ <E>｛
       size++; 
 //     }//end of else
   }
-  
-  public E peek(){
-    if (size == 0) {
-      return null;
-    } else {
-      return array[0];
-    }
-  }
-  
-  public E poll(){
-    if (size == 0) {
-      return null;
-    } else { 
-      int res = array[0];  //first, store the output;
-      array[0] = array[size - 1];
-      percolateDown(0);
-      size--;
-      return res;
-    }
-  }
 
-  void swap (E[] array, int left, int right) {
-    E tmp = array[left];
-    array[left] = array[right];
-    array[right] = tmp;
-  }
-  
-  private void percolateDown(int idx) {
-    
-    while(idx * 2 + 1 < size) {
-      int idxL = idx * 2 + 1;
-      int idxR = idx * 2 + 2;
-      int min = idxL;
-      if (idxR < size && array[idxR].compareTo(array[idxL]) < 0) {
-        min = idxR;
-      }
-      if (arary[idx].compareTo(array[min]) > 0) {
-        swap(array, idx, min);
-        idx = min;
-      } else {
-        break;
-      } 
-    }
-  }
+	public E peek() {
+		if (size == 0) {
+			return null;
+		} else {
+			return (E)array[0];
+		}
+	}
 
-  private void percolateUp(int idx) {
-    while (idx > 0) {
-      int idxP = (idx - 1) / 2;
-      if (array[idx].compareTo(array[idxP]) < 0) {
-        swap(array, idx, idxp);
-        idx = idxP;
-      } else {
-        break;
-      }
-    }
-  }
-  
-  void heapify () { 
-    int lastP = (array.length - 1) / 2;
-    for (int i = lastP; i >=0; i--) {
-      percolateDown(array[i]);
-    }
-  }
-//   private resize(); 
-  
-｝// end of class
+	public E poll() {
+		if (size == 0) {
+			return null;
+		} else {
+			E res = (E)array[0]; // first, store the output;
+			array[0] = array[size - 1];
+			percolateDown(0);
+			size--;
+			return res;
+		}
+	}
+
+	void swap(E[] array, int left, int right) {
+		E tmp = array[left];
+		array[left] = array[right];
+		array[right] = tmp;
+	}
+
+	private void percolateDown(int idx) {
+
+		while (idx * 2 + 1 < size) {
+			int idxL = idx * 2 + 1;
+			int idxR = idx * 2 + 2;
+			int min = idxL;
+			if (idxR < size && ((E)array[idxR]).compareTo(((E)array[idxL])) < 0) {
+				min = idxR;
+			}
+			if (((E)array[idx]).compareTo(((E)array[min])) > 0) {
+				swap(((E[])array), idx, min);
+				idx = min;
+			} else {
+				break;
+			}
+		}
+	}
+
+	private void percolateUp(int idx) {
+		while (idx > 0) {
+			int idxP = (idx - 1) / 2;
+			if (((E)array[idx]).compareTo(((E)array[idxP])) < 0) {
+				swap(((E[])array), idx, idxP);
+				idx = idxP;
+			} else {
+				break;
+			}
+		}
+	}
+
+	void heapify() {
+		int lastP = (array.length - 1) / 2;
+		for (int i = lastP; i >= 0; i--) {
+			percolateDown(i);
+		}
+	}
+	// private resize();
+
+}// end of class
