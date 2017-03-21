5228cc02e9d6d19cd12618c272c7d61ff0826cee
huailian123 <huailian123@gmail.com>
2017-03-20 22:43:47 -0700

    Update heapImplement.java

--- heapImplement.java
+++ heapImplement.java
@@ -1,35 +1,34 @@
-public class MinHeap <E>｛
+public class LianPQ <E>｛
   private E[] array;
   private size;
   
   
-  public int size() {
+  public int size() {  //date length stored inside
+    return size;
   }
   public int isEmpty() {
+    return size == 0;
   }
-  public MinHeap(int initSize) {
-    array = new E[initSize];
+  public LianPQ(int inputSize) {
+    array = new E[inputSize];
     size = 0;
   }
   
+  public LianPQ(E[] inputArray) {
+    array = inputArray;
+    size = inputArray.length;
+    heapify();
+  }
+  
   public offer(E elem){
-    if (size == array.length) {
-      resize();
-    } else {
+//     if (size == array.length) {
+//       resize();
+//     } else {
       int curIdx = size;
       array[curIdx] = elem;
-      while (curIdx > 0) {
-        int paIdx = (size - 1) / 2;
-        if (array[curIdx] < array[paIdx]) {  // swap it to it's parent
-          int temp = array[curIdx];
-          array[curIdx] = array[paIdx];
-          array[paIdx] = temp;
-        } else {
-          break;
-        }
-      } // end of while
+      percolateUp(curIdx);
       size++; 
-    }//end of else
+//     }//end of else
   }
   
   public E peek(){
@@ -46,24 +45,54 @@ public class MinHeap <E>｛
     } else { 
       int res = array[0];  //first, store the output;
       array[0] = array[size - 1];
-      int curIdx = 0;
-      while (curIdx < size - 1) {
-        int lChildIdx = 2 * curIdx + 1;
-        int rChildIdx = 2 * curIdx + 2;
-        int min = Math.min(array[lChildIdx], array[lChildIdx]);
-        if (array[curIdx] > min) {
-          int minIdx = array[lChildIdx] < array[lChildIdx] ? lChildIdx : rChildIdx;
-          array[minIdx] = array[curIdx];
-          array[curIdx] = min;
-        } else {
-          break;
-        }
-      }
+      percolateDown(0);
       size--;
       return res;
     }
   }
+
+  void swap (E[] array, int left, int right) {
+    E tmp = array[left];
+    array[left] = array[right];
+    array[right] = tmp;
+  }
   
-  private resize(); 
+  private void percolateDown(int idx) {
+    
+    while(idx * 2 + 1 < size) {
+      int idxL = idx * 2 + 1;
+      int idxR = idx * 2 + 2;
+      int min = idxL;
+      if (idxR < size && array[idxR].compareTo(array[idxL]) < 0) {
+        min = idxR;
+      }
+      if (arary[idx].compareTo(array[min]) > 0) {
+        swap(array, idx, min);
+        idx = min;
+      } else {
+        break;
+      } 
+    }
+  }
+
+  private void percolateUp(int idx) {
+    while (idx > 0) {
+      int idxP = (idx - 1) / 2;
+      if (array[idx].compareTo(array[idxP]) < 0) {
+        swap(array, idx, idxp);
+        idx = idxP;
+      } else {
+        break;
+      }
+    }
+  }
+  
+  void heapify () { 
+    int lastP = (array.length - 1) / 2;
+    for (int i = lastP; i >=0; i--) {
+      percolateDown(array[i]);
+    }
+  }
+//   private resize(); 
   
 ｝// end of class
